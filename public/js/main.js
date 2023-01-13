4bdd6acccb1e00acbc9ff84c4d4cd646349453ff
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-13 09:27:53 +0100

    Affichage du premier paragraphe avec tartine

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,6 @@
 
 
 // ### 1. Affiche le contenu textuel du premier paragraphe contenant la class "tartine", utilise une méthode pour récupérer un élémént via sa class
+console.log(document.getElementsByClassName('tartine')[0].textContent);
 // ### 2. Pareil que le précédent mais cette fois si c'est via l'id "toast"
 // ### 3. Affiche le dernier élément ayant la class tartine ( pas son contenu, l'élément entier)
